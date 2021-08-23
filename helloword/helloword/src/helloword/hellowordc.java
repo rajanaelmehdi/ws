ab964df45edeac5d6f2372f9550003dc07d1638a
rajana elmehdi <era.ht21@gmail.com>
2021-08-23 18:29:36 +0200

    delpoy/

---
+++ helloword/helloword/src/helloword/hellowordc.java
@@ -0,0 +1,13 @@
+package helloword;
+
+public class hellowordc {
+
+	private static String name; 
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+  System.out.println("mehdi rajana");
+    	System.out.println("nu + ll =" + " " + name);
+    }
+
+} 
---
+++ sans_ide/HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World, test1, belaid");
+        System.out.println("Hello, World, test1, belaid");
+        System.out.println("Hello, World, test1, belaid");
+        System.out.println("Hello, World, test1, belaid");
+        System.out.println("Hello, World, test1, belaid");
+        System.out.println("Hello, World, test1, belaid");
+        System.out.println("Hello, World, test1, belaid");
+        System.out.println("Hello, World, test1, belaid");
+    }
+
+}
